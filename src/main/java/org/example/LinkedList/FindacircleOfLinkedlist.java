4465af69f31061fe01833ce25a584ed1ba86f41f
Ganesh <ganeshdadi64@gmail.com>
2025-01-28 12:04:44 +0530

    changes moved

--- src/main/java/org/example/LinkedList/FindacircleOfLinkedlist.java
+++ src/main/java/org/example/LinkedList/FindacircleOfLinkedlist.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 public class FindacircleOfLinkedlist {
     public static void main(String[] args) {
         LinkedList ll=new LinkedList(1);
-        
         ll.next=new LinkedList(2);
         ll.next.next=new LinkedList(3);
         ll.next.next.next=new LinkedList(4);
