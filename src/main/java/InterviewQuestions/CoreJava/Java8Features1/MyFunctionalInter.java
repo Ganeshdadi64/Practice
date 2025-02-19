86d56fe698924668dd94e94f3c33e886ef9d79b1
Ganesh <ganeshdadi64@gmail.com>
2025-02-20 02:07:07 +0530

    changes moved

---
+++ src/main/java/InterviewQuestions/CoreJava/Java8Features1/MyFunctionalInter.java
@@ -0,0 +1,11 @@
+package InterviewQuestions.CoreJava.Java8Features1;
+
+
+import java.lang.FunctionalInterface;
+
+@FunctionalInterface
+public interface MyFunctionalInter {
+
+    void sayHello();
+
+}
